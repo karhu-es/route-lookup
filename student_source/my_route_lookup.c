9ba7ed135492f367b148c70e794d4b30ef7eb5de
marcoscasado <marcoscasado6@hotmail.com>
2019-03-31 20:19:26 +0200

    Parciarlmente arreglado > 24

--- student_source/my_route_lookup.c
+++ student_source/my_route_lookup.c
@@ -2,9 +2,8 @@
 #include "utils.h"
 #include "my_route_lookup.h"
 
-#define TBL24_Size 16777216
-#define TBLong_Block_Size 256
-#define Entry_Size 16
+#define TBL24_SIZE 16777216
+#define BLOCK_SIZE 256
 #define CTE 1000
 
 
@@ -24,7 +23,7 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  uint16_t * tbl24 = (uint16_t *)calloc(TBL24_Size, Entry_Size);
+  uint16_t * tbl24 = (uint16_t *)calloc(TBL24_SIZE, sizeof(uint16_t));
   uint16_t * tblong = (uint16_t *)malloc(0);
   if(tbl24 == NULL || tblong == NULL){
     perror("malloc");
@@ -41,6 +40,7 @@ int main(int argc, char *argv[]) {
 
   while(lectura != EOF){
     lectura =  readFIBLine(&prefix, &prefixLength, &outInterface);
+	printf("Lectura de entrada - Prefijo:%d/%d >> %d\n", prefix, prefixLength, outInterface);
     if(lectura != 0){
       perror("readFIBLine");
       break;
@@ -49,51 +49,46 @@ int main(int argc, char *argv[]) {
 
 
     if(prefixLength == 0){
-      //tbl24 = outInterface >> 0;
       puts("Prefijo 0");
+	  
     }else if(prefixLength <=  24){
-      table_entry = (uint16_t) outInterface;
-      n_options = pow(2, 24-prefixLength);
-      for(i = 0; i < n_options; i++){
-        memcpy(&tbl24[table_position + i], &table_entry, sizeof(uint16_t));
-      }
-      printf("Valor de la tabla----------------------%d\n", tbl24[table_position] );
+		puts("Prefijo <= 24");
+		table_entry = (uint16_t) outInterface;
+		n_options = pow(2, 24-prefixLength);
+		for(i = 0; i < n_options; i++){
+		memcpy(&tbl24[table_position + i], &table_entry, sizeof(uint16_t));
+		}
+		//printf("ADDED TBL24 in Range[%d , %d] >> %d\n",table_position, table_position + n_options, tbl24[table_position] );
 
-    }else if(prefixLength >  24){
-      puts("Prefijo > 24");
-      if ( (tbl24[table_position]   | (uint16_t) pow(2,15)) == tbl24[table_position]  ){
+    }  else if(prefixLength > 24){
+		puts("Prefijo > 24");
+		
+		if ( (tbl24[table_position] ^ (uint16_t) pow(2,15)) == (uint16_t)0  ){
 
-          n_options = pow(2, 32-prefixLength);
-          table_entry = outInterface;
-          table_position = (tbl24[table_position] ^ (uint16_t) pow(2,15)) - CTE;
-          for(j = n_options - 1; j < 2*n_options -1; j++ ){
-            memcpy(&tblong[table_position + j], &table_entry, sizeof(uint16_t));
-          }
-
-          ////Esto es para mostrar en pantalla el resultado
-          for(i = 0; i < TBLong_Block_Size; i++){
-            printf("Valor de la tabla----------------------%d\n", tblong[table_position +i ] );
-          }
+			n_options = pow(2, 32-prefixLength);
+			table_entry = outInterface;
+			table_position = ((tbl24[table_position] ^ (uint16_t) pow(2,15)) - CTE) * BLOCK_SIZE;
+			for(j = n_options - 1; j < 2*n_options -1; j++ ){
+				memcpy(&tblong[table_position + j], &table_entry, sizeof(uint16_t));
+			}
 
         }else{
-          puts("Aqui en la opcion 0 con algo");
+			
           aux = tbl24[table_position];
           n_options = pow(2, 32-prefixLength);
-          tblong = realloc(tblong, TBLong_Block_Size);
-
+		  
+		  table_entry = CTE + nblocks_tblong++;
+          table_entry = (uint16_t)(table_entry | (uint16_t) pow(2,15));
+          memcpy(&tbl24[table_position], &table_entry, sizeof(uint16_t));
+          table_entry = outInterface;
+		  
+          tblong = realloc(tblong, BLOCK_SIZE);
           if(tblong == NULL){
             perror("realloc");
           }
 
-          table_entry = CTE + nblocks_tblong*TBLong_Block_Size;
-          table_entry = (uint16_t)(table_entry | (uint16_t) pow(2,15));
-          nblocks_tblong ++;
-          memcpy(&tbl24[table_position], &table_entry, sizeof(uint16_t));
-          printf("Entrada escrita en posicion %d >> %d\n",table_position, tbl24[table_position] );
-          table_entry = outInterface;
-
-          for(i = 0; i < TBLong_Block_Size; i++){
-            table_position = nblocks_tblong*TBLong_Block_Size;
+          for(i = 0; i < BLOCK_SIZE; i++){
+            table_position = nblocks_tblong*BLOCK_SIZE;
             memcpy(&tblong[table_position + i], &aux, sizeof(uint16_t));
 
           }
@@ -102,30 +97,24 @@ int main(int argc, char *argv[]) {
           }
 
           ////Esto es para mostrar en pantalla el resultado
-          for(i = 0; i < TBLong_Block_Size; i++){
-            table_position = nblocks_tblong*TBLong_Block_Size;
-            printf("Valor de la tabla----------------------%d\n", tblong[table_position +i ] );
-
-
+          for(i = 0; i < BLOCK_SIZE; i++){
+            table_position = nblocks_tblong*BLOCK_SIZE;
+            //printf("Valor de la tabla----------------------%d\n", tblong[table_position +i ] );
           }
         }
-        //printf("Valor de la tabla----------------------%d\n", tblong[table_position] );
-
+	
     }else{
       puts("Reading prefix length failed");
       break;
     }
-
-      printf("%d, %d, %d, %d\n", prefix, prefixLength, outInterface, tbl24[table_position]);
+      
   }
-  for(i = 0; i <= 16; i++){
-    printf("%d\n", tbl24[i]);
-  }
-
+  
   printf("hey lets delete memory\n" );
   //printf("%d, %d, %d\n", prefix, prefixLength, outInterface );
   free(tbl24);
   free(tblong);
   freeIO();
+  printMemoryTimeUsage();
   return 0;
 }
